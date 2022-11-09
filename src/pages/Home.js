fd2267eb615d239a76fb4e5eadfa5a25db54c57c
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-09 23:55:21 +0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import dBackground from '../assets/home/background-home-desktop.jpg'
 import tBackground from '../assets/home/background-home-tablet.jpg'
 import mBackground from '../assets/home/background-home-mobile.jpg'
+import HomeComponent from '../components/HomeComponent'
 
 const Home = ({sethash}) => {
   sethash('#/')
@@ -10,6 +11,7 @@ const Home = ({sethash}) => {
         <img className='hidden dp:block w-full h-full' alt='background' src={dBackground} />
         <img className='hidden md:block dp:hidden w-full h-full' src={tBackground} alt='background' />
         <img className='md:hidden  w-full h-full' src={mBackground} alt='background' />
+        <HomeComponent/>
     </div>
   )
 }
