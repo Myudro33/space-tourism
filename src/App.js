46b06452bda8b863d91692e753ee2c90913cbf1a
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-09 23:55:34 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Crew from "./pages/Crew";
@@ -8,15 +8,18 @@ import Technology from "./pages/Technology";
 import SideMenu from "./components/SideMenu";
 
 const App = () => {
-  const [hash, sethash] = useState()
+  const [hash, sethash] = useState();
   const [icon, seticon] = useState(false);
   return (
     <div className="w-full h-screen">
-        <Navbar hash={hash} icon={icon} seticon={seticon} />
-        <SideMenu icon={icon} seticon={seticon} />
+      <Navbar hash={hash} icon={icon} seticon={seticon} />
+      <SideMenu hash={hash} icon={icon} seticon={seticon} />
       <Routes>
         <Route path="/" element={<Home sethash={sethash} />} />
-        <Route path="/destinations" element={<Destinations sethash={sethash} />} />
+        <Route
+          path="/destinations"
+          element={<Destinations sethash={sethash} />}
+        />
         <Route path="/crew" element={<Crew sethash={sethash} />} />
         <Route path="/technology" element={<Technology sethash={sethash} />} />
       </Routes>
