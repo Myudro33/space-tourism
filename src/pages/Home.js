9355e9b6b5522318b244a182f4be3db8ee3729d0
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-09 23:21:08 +0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,11 @@ import dBackground from '../assets/home/background-home-desktop.jpg'
 import tBackground from '../assets/home/background-home-tablet.jpg'
 import mBackground from '../assets/home/background-home-mobile.jpg'
 
-const Home = () => {
+const Home = ({sethash}) => {
+  sethash('#/')
   return (
     <div className='w-full h-screen'>
-        <img className='hidden dp:block relative w-full h-full' alt='background' src={dBackground} />
+        <img className='hidden dp:block w-full h-full' alt='background' src={dBackground} />
         <img className='hidden md:block dp:hidden w-full h-full' src={tBackground} alt='background' />
         <img className='md:hidden  w-full h-full' src={mBackground} alt='background' />
     </div>
