510c7e6c774db3489d0d1268275d8f9f7e4d6314
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-09 14:43:58 +0400

    set background image

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import dBackground from '../assets/home/background-home-desktop.jpg'
+import tBackground from '../assets/home/background-home-tablet.jpg'
+import mBackground from '../assets/home/background-home-mobile.jpg'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen'>
+        <img className='hidden dp:block relative w-full h-full' alt='background' src={dBackground} />
+        <img className='hidden md:block dp:hidden w-full h-full' src={tBackground} alt='background' />
+        <img className='md:hidden  w-full h-full' src={mBackground} alt='background' />
+    </div>
+  )
+}
+
+export default Home
