d17c8346cbf58acd9f90317a99d06bf1ff9a5afa
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-10 15:46:58 +0400

    Update Crew.js

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from "react";
+import bgDesktop from "../assets/crew/background-crew-desktop.jpg";
+import bgTablet from "../assets/crew/background-crew-tablet.jpg";
+import bgMobile from "../assets/crew/background-crew-mobile.jpg";
+import CrewComponent from "../components/CrewComponent";
 
-const Crew = ({sethash}) => {
-  sethash('#/crew')
+const Crew = ({ sethash }) => {
+  sethash("#/crew");
   return (
-    <div>Crew</div>
-  )
-}
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <img
+        className="hidden absolute dp:block w-full h-full"
+        alt="background"
+        src={bgDesktop}
+      />
+      <img
+        className="hidden absolute md:block dp:hidden w-full h-full"
+        src={bgTablet}
+        alt="background"
+      />
+      <img
+        className="md:hidden absolute w-full h-full"
+        src={bgMobile}
+        alt="background"
+      />
+      <h1 className="z-10 md:w-[95%] dp:w-[70%] text-start mt-28 flex text-[white] text-2xl">
+        <p className="text-[gray] mr-4 font-semibold">02</p>MEET YOUR CREW
+      </h1>
+      <CrewComponent/>
+    </div>
+  );
+};
 
-export default Crew
+export default Crew;
